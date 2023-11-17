5d2ed372fa5e59890e767dacf600b8807b81c2f8
vanessateixeira <ne04_teixeira@hotmail.com>
2023-11-17 00:55:10 -0300

    exercicios-resolvidos

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const numeros = [4,7,8,9,3,1,6]
+const nomes = ['vanessa', 'teddy', 'mãe']
+const tipos = ['teddy', 18, false]
+
+console.log(numeros)
+console.log(nomes)
+console.log(tipos)
---
+++ exercicio2/script.js
@@ -0,0 +1,31 @@
+function lista (array1, array2, array3){
+
+const nomes = ['vanessa', 'teddy', 'mãe']
+const numeros = [2, 7, 9, 0, 1, 4,5]
+const tipos = ['teddy', 4, true]
+
+console.log(nomes.length)
+console.log(numeros.length)
+console.log(tipos.length)
+
+
+}
+
+lista()
+
+function lista2(array1, array2, array3){
+    const nomes = ['vanessa', 'teddy', 'mãe']
+    const numeros = [2, 7, 9, 0, 1, 4,5]
+    const tipos = ['teddy', 4, true]
+
+    console.log(nomes[0])
+    console.log(numeros[1])
+    console.log(tipos[2])
+
+    console.log(nomes.includes('teddy'))
+    console.log(tipos.includes(5))
+
+
+}
+
+lista2()
